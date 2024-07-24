add98a9ca074796689a02b25e849f18dc206a439
Murilo Fank <murilo.fank@caf.io>
2024-07-24 15:36:38 -0300

    last try

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,6 @@ const port = process.env.PORT || 3000;
 // Initialize Sequelize
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
   dialect: 'postgres'
 });
 
